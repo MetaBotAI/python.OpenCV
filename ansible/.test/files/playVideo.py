5f377629d953846e797eb862fa3cc2fc9f666727
metabot <metabot@ai.com>
2022-09-14 15:46:39 -0400

    try

---
+++ ansible/.test/files/playVideo.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+capture = cv.VideoCapture('try.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
+cv.waitKey(0)
